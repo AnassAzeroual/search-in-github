fc913f1f0b23c90da93343b9ad5dfdec5ade3a6e
AnassAzeroual <azeroual.anass2015@gmail.com>
2020-04-30 16:01:51 +0000

    2 files modified, 4 added

--- src/app/search-handling/search-handling.component.ts
+++ src/app/search-handling/search-handling.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {plainToClass} from "class-transformer";
+import {RepositoryResult} from "./model/RepositoryResult";
 
 @Component({
   selector: 'search-handling',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchHandlingComponent implements OnInit {
 
-  constructor() { }
+repositoryResult: RepositoryResult;
+
+  constructor (private http: Http) {
+  }
+
+  search(term: string) {
+    this.http
+        .get("https://api.github.com/search/repositories?q=" + term)
+        .map(response => {
+            const repositoryResult = plainToClass(RepositoryResult, response.json() as Object);
+            console.log("loaded and transformed repositories: ", repositoryResult);
+            console.log("you can use class methods: ", repositoryResult.repositoryUrls);
+            return repositoryResult;
+        })
+        .subscribe(repositoryResult => {
+            this.repositoryResult = repositoryResult;
+        });
+  }
 
   ngOnInit() {
   }
