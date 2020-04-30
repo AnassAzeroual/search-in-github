53384505a31f22e23fd647d995a757c6a10a7d31
AnassAzeroual <azeroual.anass2015@gmail.com>
2020-04-30 15:41:08 +0000

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SearchHandlingComponent } from './search-handling/search-handling.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SearchHandlingComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/search-handling/search-handling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-handling',
+  templateUrl: './search-handling.component.html',
+  styleUrls: ['./search-handling.component.css']
+})
+export class SearchHandlingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
